da45d9b311e7dfb96136bf1b14b04d95a06cf7bd
DevCloudy-max <jayprajapati491@gmail.com>
2025-03-02 12:03:48 +0530

    Commit For Swagger Documention

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -8,10 +8,148 @@ use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Passport\Token;
+
+/**
+ * @OA\Info(
+ *    title="Laravel v11 Passport API Documentation",
+ *    version="1.0.0",
+ * )
+ */
+
 class ApiController extends Controller
 {
+
+
+    // anotation to create Swagger Docume documentation
+    /**
+     * @OA\Post(
+     * path="/api/register",
+     * operationId="Register",
+     * tags={"Register"},
+     * summary="User Register",
+     * description="User Register here",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"name","email", "password", "password_confirmation"},
+     *               @OA\Property(property="name", type="text", example="Sanjay"),
+     *               @OA\Property(property="email", type="text" ,example="sajay@gmail.com"),
+     *               @OA\Property(property="password", type="password", example="123456"),
+     *               @OA\Property(property="password_confirmation", type="password" ,example="123456"),
+     *            ),
+     *        ),
+     *    ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Register Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Register Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     * )
+     *
+     * @OA\Post(
+     *     path="/api/login",
+     *     operationId="Login",
+     *     tags={"Login"},
+     *     summary="User Login",
+     *     description="User Login here",
+     *     @OA\RequestBody(
+     *         required=true,
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"email", "password"},
+     *               @OA\Property(property="email", type="string", example="sanjay@gmail.com"),
+     *               @OA\Property(property="password", type="string", example="123456"),
+     *            ),
+     *        ),
+     *        @OA\MediaType(
+     *            mediaType="application/json",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"email", "password"},
+     *               @OA\Property(property="email", type="string", example="sanjay@gmail.com"),
+     *               @OA\Property(property="password", type="string", example="123456"),
+     *            ),
+     *        ),
+     *    ),
+     *    @OA\Response(
+     *        response=201,
+     *        description="Login Successfully",
+     *        @OA\JsonContent()
+     *    ),
+     *    @OA\Response(
+     *        response=200,
+     *        description="Login Successfully",
+     *        @OA\JsonContent()
+     *    ),
+     *    @OA\Response(
+     *        response=422,
+     *        description="Unprocessable Entity",
+     *        @OA\JsonContent()
+     *    ),
+     *    @OA\Response(response=400, description="Bad request"),
+     *    @OA\Response(response=404, description="Resource Not Found"),
+     * )
+     * 
+     *
+     * @OA\Get(
+     *     path="/api/profile",
+     *     operationId="getProfile",
+     *     tags={"Profile"},
+     *     summary="Get user profile",
+     *     description="Retrieve user profile information.",
+     *     security={{"bearerAuth":{}}},
+     *     @OA\Parameter(
+     *         name="Authorization",
+     *         in="header",
+     *         description="Authorization Token",
+     *         required=true,
+     *         @OA\Schema(
+     *             type="string",
+     *             default="Bearer your_access_token_here"
+     *         )
+     *     ),
+     *     @OA\Response(
+     *         response=200,
+     *         description="Successful operation",
+     *         @OA\JsonContent()
+     *     ),
+     *     @OA\Response(
+     *         response=401,
+     *         description="Unauthorized"
+     *     )
+     * )
+     *
+     * @OA\SecurityScheme(
+     *     securityScheme="bearerAuth",
+     *     type="http",
+     *     scheme="bearer",
+     *     bearerFormat="JWT"
+     * )
+   
+     */
+
+
+
+
     public function register(Request $request)
-    {    
+    {
         //validation
         $request->validate([
             'name' => 'required|string',
@@ -19,7 +157,7 @@ class ApiController extends Controller
             'password' => 'required|confirmed',
         ]);
         //create user
-       User::create([
+        User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => bcrypt($request->password),
@@ -28,7 +166,7 @@ class ApiController extends Controller
         return response()->json([
             'status' => true,
             'message' => 'User created successfully',
-        'data' => []
+            'data' => []
         ]);
     }
 
@@ -43,74 +181,72 @@ class ApiController extends Controller
         //email check
         $user = User::where('email', $request->email)->first();
         //password
-        if(!empty($user)){
-            
-                if(Hash::check($request->password, $user->password)){
-                    $token = $user->createToken('auth_token')->accessToken;
-                    return response()->json([
-                        'status' => true,
-                        'message' => 'User logged in successfully',
-                        'token' => $token,
-                        'data' => []
-                    ]);
-                }else{
-                    return response()->json([
-                        'status' => false,
-                        'message' => "password didn't match",
-                        'data' => []
-                    ]);
-                }
+        if (!empty($user)) {
 
-        }else{
+            if (Hash::check($request->password, $user->password)) {
+                $token = $user->createToken('auth_token')->accessToken;
+                return response()->json([
+                    'status' => true,
+                    'message' => 'User logged in successfully',
+                    'token' => $token,
+                    'data' => []
+                ]);
+            } else {
+                return response()->json([
+                    'status' => false,
+                    'message' => "password didn't match",
+                    'data' => []
+                ]);
+            }
+        } else {
             return response()->json([
                 'status' => false,
                 'message' => 'Invalid email or password',
                 'data' => []
             ]);
         }
-    
+    }
+
+    //auth token
+
+
+
+    public function profile()
+    {
+        $user = Auth::user();
+
+        if (!$user) {
+            return response()->json([
+                "status" => false,
+                "message" => "Unauthorized access",
+                "data" => []
+            ], 401);
         }
 
-        //auth token
-    
-
-    
-public function profile()
-{
-    $user = Auth::user();
-
-    if (!$user) {
         return response()->json([
-            "status" => false,
-            "message" => "Unauthorized access",
-            "data" => []
-        ], 401);
+            "status" => true,
+            "message" => "Profile information",
+            "data" => $user
+        ]);
     }
 
-    return response()->json([
-        "status" => true,
-        "message" => "Profile information",
-        "data" => $user
-    ]);
-}
 
+    public function logout()
+    {
+        $user = Auth::user(); // Get the authenticated user
 
-public function logout()
-{
-    $user = Auth::user(); // Get the authenticated user
+        if (!$user) {
+            return response()->json([
+                "status" => false,
+                "message" => "User not authenticated"
+            ], 401);
+        }
 
-    if (!$user) {
+        // Revoke the current access token
+        Token::where('user_id', $user->id)->where('revoked', false)->update(['revoked' => true]);
         return response()->json([
-            "status" => false,
-            "message" => "User not authenticated"
-        ], 401);
+            "status" => true,
+            "message" => "User logged out successfully"
+        ]);
     }
-
-    // Revoke the current access token
-    Token::where('user_id', $user->id)->where('revoked', false)->update(['revoked' => true]);
-    return response()->json([
-        "status" => true,
-        "message" => "User logged out successfully"
-    ]);
-}
 }
